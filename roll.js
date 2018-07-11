8db4bb97297a06d9be0635926caf66a60a94784f
boomyao <zhangyaoruo@outlook.com>
2018-07-11 11:02:48 +0800

    save

--- roll.js
+++ roll.js
@@ -1,37 +1,55 @@
-function ScoreRoll(paperElem) {
-  if (!paperElem) throw "must suport current element."
-  paperElem.style.overflow = 'auto';
-  this.paperElem = paperElem;
-  this.rowWidth = 16;
-  this.rowHeight = 30;
+function ScoreRoll(element) {
+  this.element = element || document.scrollingElement;
+  this.bpr = 16; // 每行音数量
+  this.hpr = 30; // 每行高度
   this.cursor = 0;
   this.currentRow = 0;
   this.rowCount = 0;
-  this.speed = 2;
+  this.bpm = 80;
+  this.scrolling = false;
+  this.frameCap = 0;
+
+  this.frame = this.frame.bind(this);
 }
 
 ScoreRoll.prototype.play = function () {
-  this.playTimer = setInterval(() => {
-    this.next();
-  }, 1000)
+  this.scrolling = true;
+  requestAnimationFrame(this.frame);
 }
 
 
 ScoreRoll.prototype.next = function () {
-  this.cursor += this.speed;
-  this.cursor = this.cursor % this.rowWidth;
+  this.cursor = ++this.cursor % this.bpr;
   if (this.cursor) return;
-  if (this.rowCount && this.currentRow++ < this.rowCount || 
-    !this.rowCount && this.paperElem.scrollTop + this.paperElem.clientHeight < this.paperElem.scrollHeight ) {
-    this.paperElem.scrollBy({
-      top: this.rowHeight
+  if (!this.hadAtBottomWithRow()) {
+    this.rowCount++;
+    this.element.scrollBy({
+      top: this.hpr
     })
-  } else {
-    this.pause();
   }
 }
 
 ScoreRoll.prototype.pause = function () {
-  console.log('pause')
-  if (this.playTimer) clearInterval(this.playTimer);
+  this.scrolling = false;
+}
+
+ScoreRoll.prototype.frame = function () {
+  var moveH = this.hpr/(this.bpr/(this.bpm/(60*60)));
+  if (Math.floor(this.frameCap) < Math.floor(this.frameCap+=moveH)) this.element.scrollTop+=1;
+  if(this.hadAtBottom()) this.scrolling = false; 
+  if (this.scrolling) {
+    requestAnimationFrame(this.frame);
+  }
+}
+
+ScoreRoll.prototype.hadAtBottom = function() {
+  return this.element.scrollTop + this.element.clientHeight === this.element.scrollHeight;
+}
+
+ScoreRoll.prototype.hadAtBottomWithRow = function() {
+  if (this.rowCount) {
+    return this.currentRow + 1 === this.rowCount;
+  } else {
+    return this.element.scrollTop + this.element.clientHeight === this.element.scrollHeight;
+  }
 }
